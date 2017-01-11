8622e8996a32bfaa2933064318b93298a8319f75
irrelon <rob@irrelon.com>
2017-01-11 21:08:33 +0000

    Fixed bug in using boardWidth for both width and height, destroy old board before creating new one if it exists.

--- app/Controls.js
+++ app/Controls.js
@@ -10,7 +10,7 @@ app.module('Controls', function (Board) {
 			
 			// Create a new board based on the width and height selected by the user
 			width = parseInt(view.querySelector('#boardWidth').value, 10);
-			height = parseInt(view.querySelector('#boardWidth').value, 10);
+			height = parseInt(view.querySelector('#boardHeight').value, 10);
 			
 			if (!width || width < 10) {
 				return alert('Please select a board width greater or equal to 10!');
@@ -18,6 +18,12 @@ app.module('Controls', function (Board) {
 			
 			if (!height || height < 10) {
 				return alert('Please select a board height greater or equal to 10!');
+			}
+			
+			// Destroy any old board we might have
+			if (app.state.board) {
+				app.state.board.destroy();
+				delete app.state.board;
 			}
 			
 			// We have a width and height, create the board with the target
