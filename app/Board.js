6671638f3bc2479c145d82600dfbc0f041bff228
irrelon <rob@irrelon.com>
2017-01-11 21:07:09 +0000

    Added board.destroy() method

--- app/Board.js
+++ app/Board.js
@@ -18,6 +18,17 @@ app.module('Board', function (Cell) {
 		
 		// Remember the target element we will render to
 		this._renderTargetSelector = targetElementSelector;
+		
+		// Grab the render element
+		this._elem = document
+			.querySelector(this._renderTargetSelector);
+		
+		// Calculate the width and height in CSS of the board container element
+		// ** with more time, I would have read the CSS styles in browser and used
+		// that value instead of hard-coding 10 here ** - we add 2 to the end as the
+		// board has a border of 1px on both sides of an axis === 2px.
+		this._elem.style.width = String((width * 10) + 2); // Cells are 10 pixels width
+		this._elem.style.height = String((height * 10) + 2); // Cells are 10 pixels height
 	};
 	
 	/**
@@ -25,6 +36,9 @@ app.module('Board', function (Cell) {
 	 */
 	Board.prototype.init = function () {
 		var x, y;
+		
+		// Clear the board's HTML
+		this._elem.innerHTML = "";
 		
 		// Loop over x and y axis and generate a new blank
 		// cell instance for each cell on the board
@@ -37,6 +51,17 @@ app.module('Board', function (Cell) {
 				this._cells[x][y] = new Cell(this._renderTargetSelector, x, y);
 			}
 		}
+	};
+	
+	/**
+	 * Destroys all cells in the board and removes any reference to them.
+	 */
+	Board.prototype.destroy = function () {
+		// Clear the board's HTML
+		this._elem.innerHTML = "";
+		
+		// Clear references to all cells
+		this._cells = [];
 	};
 	
 	return Board;
